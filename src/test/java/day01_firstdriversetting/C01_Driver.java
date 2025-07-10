73c8f250a413e50d0b6c86729b4ef288687eab36
HercaiSevvalErdem <hercaierdem27@gmail.com>
2025-07-10 23:47:07 +0300

    driver

--- src/test/java/day01_firstdriversetting/C01_Driver.java
+++ src/test/java/day01_firstdriversetting/C01_Driver.java
@@ -7,12 +7,19 @@ public class C01_Driver {
 
     public static void main(String[] args) {
 
+        /*
+        ChromeDriver, Seleniumdan gelen komutlari alir ve bunları chrome'un anlayabilecegi sekilde cevirir
+        Bunun icin Java dan System.setProperty() methodu ile Selenium'a ChromeDriver'in komutunu bildirerek
+        ChromeBrowser'in dogru bir sekilde baslatilmasini saglar.
+         */
+
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe" );
 
-        WebDriver driver = new ChromeDriver(); // Browser'i actik. Sonra gitmek istediğimiz url yazılır.
+        WebDriver driver = new ChromeDriver(); // Bos bir browser acat. Browser'i actik. Sonra gitmek istediğimiz url yazılır.
 
-        driver.get("https://www.google.com");
-        driver.quit();
+        driver.get("https://www.google.com"); // get() methodu ile String belirttigimiz url'e gideriz.
+        driver.quit(); // oturumu sonlandirir
+
     }
 
 }
