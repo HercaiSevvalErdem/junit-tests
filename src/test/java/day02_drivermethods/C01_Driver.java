809dc6d0c176143e4e4b0bda7ebb0abbbd665c1e
HercaiSevvalErdem <hercaierdem27@gmail.com>
2025-07-11 17:50:41 +0300

    get methods tests

--- src/test/java/day02_drivermethods/C01_Driver.java
+++ src/test/java/day02_drivermethods/C01_Driver.java
@@ -3,6 +3,7 @@ package day02_drivermethods;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class C01_Driver {
 
@@ -11,6 +12,13 @@ public class C01_Driver {
         WebDriverManager.chromedriver().setup();
 
         WebDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://google.com");
+        chromeDriver.quit();
+
+        WebDriverManager.edgedriver().setup();
+        WebDriver edgeDriver = new EdgeDriver();
+        edgeDriver.get("https://google.com");
+        edgeDriver.quit();
 
 
     }
---
+++ src/test/java/day02_drivermethods/C02_GetMethodsTest.java
@@ -0,0 +1,56 @@
+package day02_drivermethods;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C02_GetMethodsTest {
+
+    public static void main(String[] args) {
+
+        // Go to Techproeducation page https://www.techproeducation.com/
+        // Print the page title (address url)
+        // Test that the page url contains "education"
+        // Print the page handle value
+        // Test wheter the word "IT Programs" appears in the page HTML codes
+        // Close the page
+
+
+        WebDriverManager.chromedriver().setup(); // Altyapıyı kurduk.
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.techproeducation.com/");
+
+        String actualTitle = driver.getTitle();
+        System.out.println("actualTitle = " + actualTitle);
+
+        if (actualTitle.contains("IT")){
+            System.out.println("Test Passed");
+        }else {
+            System.out.println("Test Failed");
+        }
+
+        String actualUrl = driver.getCurrentUrl();
+        System.out.println("actualUrl = " + actualUrl);
+
+        if (actualUrl.contains("education")){
+            System.out.println("Test Passed");
+        }else {
+            System.out.println("Test Failed");
+        }
+
+        String windowHandle = driver.getWindowHandle();
+        System.out.println("windowHandle = " + windowHandle);
+
+        String tehchproPageSource = driver.getPageSource();
+
+        if (tehchproPageSource.contains("IT Programs")){
+            System.out.println("Test Passed");
+        }else {
+            System.out.println("Test Failed");
+        }
+
+        driver.close();
+
+    }
+}
