28ed875f7e30abc1d65313bbff02acb486e7177e
HercaiSevvalErdem <hercaierdem27@gmail.com>
2025-07-12 17:36:20 +0300

    implicitly wait

--- src/test/java/day02_drivermethods/C04_ManageMethods.java
+++ src/test/java/day02_drivermethods/C04_ManageMethods.java
@@ -2,26 +2,40 @@ package day02_drivermethods;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class C04_ManageMethods {
 
-    public static void main(String[] args) {
-
-        // Techpro sayfasına gidelim https://www.techproeducation.com/
-        // Sayfanin konumunu ve boyutunu yazdirin
-        // Sayfayi simge durumuna getirin
-        // Simge durumunda 2 saniye bekletip sayfayi maximize yapin
-        // Sayfanin konumunu ve boyutunu maximize durumunda yazdirin
-        // Sayfayi fullscreen yapin
-        // Sayfanin konumunu ve boyutunu fullscreen durumunda yazdirin
-        // Sayfayi kapatin
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
 
+        // Techpro sayfasına gidelim https://www.techproeducation.com/
+        driver.get("https://www.techproeducation.com/");
 
+        // Sayfanin boyutunu yazdirin
+        System.out.println("Sayfanin olculeri = " + driver.manage().window().getSize());
 
+        // Sayfayi simge durumuna getirin (kucult)
+        driver.manage().window().minimize();
+
+        // Simge durumunda 2 saniye bekletip sayfayi maximize yapin
+        Thread.sleep(2000);
+        driver.manage().window().maximize();
+
+        // Sayfanin boyutunu maximize durumunda yazdirin
+        System.out.println("Sayfanin olculeri = " + driver.manage().window().getSize());
+
+        // Sayfayi fullscreen yapin
+        driver.manage().window().fullscreen();
+
+        // Sayfanin boyutunu fullscreen durumunda yazdirin
+        System.out.println("Sayfanin olculeri = " + driver.manage().window().getSize());
+
+        // Sayfayi kapatin
+        driver.close();
 
     }
 }
---
+++ src/test/java/day03_implicitlywait_junit/C01_ImplicitlyWait.java
@@ -0,0 +1,69 @@
+package day03_implicitlywait_junit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.DriverManager;
+import java.time.Duration;
+
+public class C01_ImplicitlyWait {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize(); // Sayfayi buyuttuk
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15)); // Sayfadaki tum webelementlerin yuklenmessini max 15 saniye icin bekle
+
+        // Go to Techpro page
+        driver.get("https://www.techproeducation.com");
+
+        // Test that the page title contains TechPro Education
+        String expectedData = "TechPro Education";
+        String actualTitle = driver.getTitle();
+        System.out.println("actualTitle = " + actualTitle);
+
+        if (actualTitle.contains(expectedData)){
+            System.out.println("Test Passed");
+        }else {
+            System.out.println("Test Failed");
+        }
+
+        /*
+        if (actualTitle.contains("TechPro Education")) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+        }
+       */
+        // Go to Facebook page
+        driver.get("https://www.facebook.com");
+
+        // Test that the page url contains facebook
+        /*
+        String actualUrl = driver.getCurrentUrl();
+        System.out.println("actualUrl = " + actualUrl);
+        if (actualUrl.contains("facebook")) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+        }
+        */
+
+        if (driver.getCurrentUrl().contains("facebook")){
+            System.out.println("Test Passed");
+        }else {
+            System.out.println("Test Failed");
+        }
+        // Go back to Techpro page
+        driver.navigate().back();
+
+        // Test that you are back
+
+
+        // Close the page
+        driver.close();
+
+    }
+}
